d4edcbd63108b8ab230e4365f7f33fdd4f66378d
Furvent <furventt@gmail.com>
2021-05-15 12:16:32 +0200

    Add draw.ts and change figure.ts

---
+++ src/render/draw.ts
@@ -0,0 +1,50 @@
+import { Point, Rect } from "./figure"
+
+export default class Draw {
+  ctx: CanvasRenderingContext2D
+  canvasWidth: number
+  canvasHeight: number
+  color: string
+  constructor(canvasCtx: CanvasRenderingContext2D, canvasWidth: number, canvasHeight: number, color = "black") {
+          console.log("Init drawer with width " + canvasWidth + " and height " + canvasHeight)
+          this.ctx = canvasCtx
+          this.canvasWidth = canvasWidth
+          this.canvasHeight = canvasHeight
+          this.color = color
+  }
+  setColor(newColor: string) {
+      this.color = newColor
+  }
+  rect(pos: Point, rectDimension: Rect, color = this.color) {
+      this.ctx.strokeStyle = color
+      this.ctx.beginPath()
+      this.ctx.rect(pos.x, pos.y, rectDimension.width, rectDimension.height)
+      this.ctx.stroke()
+  }
+  fillRect(pos: Point, rectDimension: Rect, color = this.color) {
+      this.ctx.fillStyle = color
+      this.ctx.fillRect(pos.x, pos.y, rectDimension.width, rectDimension.height)
+  }
+  circle(pos: Point, radius: number) {
+      this.ctx.strokeStyle = this.color
+      this.ctx.beginPath()
+      this.ctx.arc(pos.x, pos.y, radius, 0, 2 * Math.PI);
+      this.ctx.stroke()
+  }
+  fillCircle(pos: Point, radius: number) {
+      this.ctx.fillStyle = this.color
+      this.ctx.beginPath()
+      this.ctx.arc(pos.x, pos.y, radius, 0, 2 * Math.PI);
+      this.ctx.fill()
+  }
+  line(posBegin: Point, posEnd: Point, color = this.color) {
+      this.ctx.strokeStyle = color
+      this.ctx.beginPath()
+      this.ctx.moveTo(posBegin.x, posBegin.y)
+      this.ctx.lineTo(posEnd.x, posEnd.y)
+      this.ctx.stroke()
+  }
+  clear() {
+      this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+  }
+}
---
+++ src/render/figure.d.ts
@@ -0,0 +1,13 @@
+export type Rect = {
+  width: number
+  height: number
+}
+
+export type Circle = {
+  radius: number
+}
+
+export type Point = {
+  x: number
+  y: number
+}
