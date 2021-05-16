46b3663f226ecb8d9463d95fcf9cb223b6e3382c
Furvent <furventt@gmail.com>
2021-05-16 11:43:24 +0200

    format project with prettier

--- src/render/figure.d.ts
+++ src/render/figure.d.ts
@@ -1,13 +1,13 @@
 export type Rect = {
-  width: number
-  height: number
-}
+  width: number;
+  height: number;
+};
 
 export type Circle = {
-  radius: number
-}
+  radius: number;
+};
 
 export type Point = {
-  x: number
-  y: number
-}
+  x: number;
+  y: number;
+};
