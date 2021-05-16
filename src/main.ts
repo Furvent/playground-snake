d7eb63e2c175e9394780d2dc6e925c1bb9ac4020
Furvent <furventt@gmail.com>
2021-05-16 11:40:21 +0200

    add main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log("WORKING");
+import Draw from "./render/draw.js";
+import Game from "./snakev1/game.js";
+
+console.log("MAIN")
+window.onload = () => {
+  init()
+}
+
+function init() {
+  console.log("Game initialisation");
+  const canvas = document.getElementById("renderer") as HTMLCanvasElement;
+  if (!canvas) console.error("No canvas loaded");
+  else {
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const draw = new Draw(ctx, canvas.width, canvas.height, "black");
+    const game = new Game(draw);
+    game.init();
+    // test(draw)
+  }
+}
+
+function test(draw: Draw) {
+  draw.line({ x: 0, y: 0 }, { x: 400, y: 450 }, "green");
+}
